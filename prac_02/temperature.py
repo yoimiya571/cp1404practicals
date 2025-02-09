3fad68bbe37d77c9c2e7f5e0a3e80a5d3327d601
asus <yuhao.guo@my.jcu.edu.au>
2025-02-09 18:05:07 +0800

    Temperature conversions

--- prac_02/temperature.py
+++ prac_02/temperature.py
@@ -16,7 +16,6 @@ while choice != "Q":
         fahrenheit = celsius * 9.0 / 5 + 32
         print(f"Result: {fahrenheit:.2f} F")
     elif choice == "F":
-        # TODO: Write this section to convert F to C and display the result
         fahrenheit = float(input("Fahrenheit : "))
         celsius = 5 / 9 * (fahrenheit - 32)
         print(f"Result: {celsius:.2f} C")
