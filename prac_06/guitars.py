45464f8b223680f657f6809ff4fdad47ab2d63bd
asus <yuhao.guo@my.jcu.edu.au>
2025-03-11 17:28:53 +0800

    Collect and display user's guitars
    Signed-off-by: asus <yuhao.guo@my.jcu.edu.au>

---
+++ prac_06/guitars.py
@@ -0,0 +1,22 @@
+def main():
+    """Collect and display user's guitars."""
+    guitars = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.")
+        name = input("Name: ")
+
+    # Display all guitars
+    print("\nThese are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+# Run the main program for guitars collection and display
+main()
