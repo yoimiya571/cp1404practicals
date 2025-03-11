e2ef6110458b3dc81bcf38e953b9153e10cfb822
asus <yuhao.guo@my.jcu.edu.au>
2025-03-11 17:29:09 +0800

    Demo code to test the ProgrammingLanguage class
    Signed-off-by: asus <yuhao.guo@my.jcu.edu.au>

---
+++ prac_06/labguage.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    """Demo code to test the ProgrammingLanguage class."""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [python, ruby, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+main()
