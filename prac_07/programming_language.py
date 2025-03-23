8ca23590e5070d50301b9bd3d2cf7dfa10d5e1cb
asus <yuhao.guo@my.jcu.edu.au>
2025-03-23 15:03:14 +0800

    Represent information about a programming language.
    Signed-off-by: asus <yuhao.guo@my.jcu.edu.au>

---
+++ prac_07/programming_language.py
@@ -0,0 +1,23 @@
+class ProgrammingLanguage:
+    """Represent information about a programming language."""
+
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
+        """Construct a ProgrammingLanguage from the given values."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
+
+    def __repr__(self):
+        """Return string representation of a ProgrammingLanguage."""
+        return (f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, "
+                f"Pointer Arithmetic={self.pointer_arithmetic}, First appeared in {self.year}")
+
+    def is_dynamic(self):
+        """Determine if language is dynamically typed."""
+        return self.typing == "Dynamic"
+
+    def supports_pointer_arithmetic(self):
+        """Determine if the language supports pointer arithmetic."""
+        return self.pointer_arithmetic
